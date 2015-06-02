3775e549c4d4718ab6a5d63121e6e5c759a0bba6
arthuroliveira1000 <tuli.oliveira@hotmail.com>
2015-06-01 22:29:31 -0300

    feat(TurboRango) - Entidades

---
+++ src/TurboRango/TurboRango.Dominio/Localizacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurboRango.Dominio
+{
+    class Localizacao
+    {
+        String bairro { get; set; }
+        String logradouro { get; set; }
+        Double latitude { get; set; }
+        Double longitude { get; set; }
+    }
+}
---
+++ src/TurboRango/TurboRango.Dominio/Restaurante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurboRango.Dominio
+{
+    class Restaurante
+    {
+        String nome { get; set; }
+        int capacidade { get; set; }
+        Categoria categoria { get; set; }
+        Contato contato { get; set; }
+        Localizacao localizacao { get; set; }
+    }
+}
