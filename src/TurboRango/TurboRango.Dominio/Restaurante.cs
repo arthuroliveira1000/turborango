1f69e84a1647e82bf6b780c9b3839dd91cfa9310
arthuroliveira1000 <tuli.oliveira@hotmail.com>
2015-06-08 17:33:35 -0300

    feat(TurboRango) - criação das entidades

--- src/TurboRango/TurboRango.Dominio/Restaurante.cs
+++ src/TurboRango/TurboRango.Dominio/Restaurante.cs
@@ -1,11 +1,11 @@
 ﻿namespace TurboRango.Dominio
 {
-    public class Restaurante
+    public class Restaurante : Entidade
     {
         public string Nome { get; set; }
         public int? Capacidade { get; set; }
-        public Categoria Categoria { get; set; }
-        public Contato Contato { get; set; }
+        public virtual Categoria Categoria { get; set; }
+        public virtual Contato Contato { get; set; }
         public Localizacao Localizacao { get; set; }
     }
 }
